6c68808c935ef87d77820d6d65b9bd88d90db5fa
rsung26 <rsung26@gmail.com>
2014-02-26 15:52:36 -0500

    single unit selection and movement experimentation

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,27 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, cr
 var background;
 var marine;
 var cursors;
+var test;
+var clickedx, clickedy;
+
+function Marine(index, game) {
+
+	this.game = game;
+	this.selected = false;
+
+ 	this.marine = game.add.sprite(100, 100, 'marine');
+ 	this.marine.anchor.setTo(0.5, 0.5);
+ 	this.marine.body.collideWorldBounds = true;
+ 	this.marine.inputEnabled = true;
+
+ 	var self = this;
+
+ 	this.marine.events.onInputDown.add(function() {
+		self.selected = true;
+    	console.log("unit now selected");
+	}, this);
+}
+
 
 function preload() {
 	// Load Assets
@@ -17,46 +38,57 @@ function create() {
 	background = game.add.tileSprite(0, 0, 2000, 2000, 'background');
     game.world.setBounds(0, 0, 2000, 2000);
 
+    // this.marines = game.add.group();
+    // this.marines.createMultiple(20, 'marine');
+    test = new Marine(0, game);
     // game.camera.setPosition(game.world.centerX - game.width/2, game.world.centerY - game.height/2);
- 	marine = game.add.sprite(100, 100, 'marine');
+
     cursors = game.input.keyboard.createCursorKeys();
 }
+
 
 function update() {
 	// Set Physics
 	// Write out Inputs Events
 
+
+	// if(game.input.mousePointer.isDown && game.input.mouse.button == 3 && test.selected == true) {
+
+	// 	game.physics.moveToPointer(test.marine, 400);
+	// 	clickedx = game.input.mousePointer.x;
+	// 	clickedy = game.input.mousePointer.y; 
+	// }
+
+	// if (Phaser.Rectangle.contains(test.marine.body, clickedx, clickedy)) {
+ //        test.marine.body.velocity.setTo(0, 0);
+ //    }
+
+
     // Mouse Camera Controls
-    if(game.input.mousePointer.x > game.width - 50 ) {
+    if(game.input.mousePointer.x > game.width) {
    		game.camera.x += 20;
     }
-    else if(game.input.mousePointer.x < 0 + 50) {
+    else if(game.input.mousePointer.x < 0) {
     	game.camera.x -= 20;
     }
-
-    if(game.input.mousePointer.y > game.height - 25) {
+    if(game.input.mousePointer.y > game.height) {
    		game.camera.y += 20;
     }
-    else if(game.input.mousePointer.y < 0 + 25) {
+    else if(game.input.mousePointer.y < 0) {
     	game.camera.y -= 20;
     }
 
     //Arrow Key Camera Controls
-	if (cursors.up.isDown)
-    {
-        game.camera.y -= 4;
+	if (cursors.up.isDown) { 
+		game.camera.y -= 4; 
+	}
+    else if (cursors.down.isDown) { 
+    	game.camera.y += 4; 
     }
-    else if (cursors.down.isDown)
-    {
-        game.camera.y += 4;
+    if (cursors.left.isDown) { 
+    	game.camera.x -= 4; 
     }
-    if (cursors.left.isDown)
-    {
-        game.camera.x -= 4;
-    }
-
-    else if (cursors.right.isDown)
-    {
-        game.camera.x += 4;
+    else if (cursors.right.isDown) { 
+    	game.camera.x += 4; 
     }
 }
