1abb80f2f37461ee9cbd6545c01e69a238b7143b
rsung26 <rsung26@gmail.com>
2014-03-01 12:22:10 -0500

    notes for today and code review/refactor

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,23 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render });
 
 var background;
-var cursors;
-var clickedx, clickedy;
-
 var marines, banelings, explosions;
-var banelings_on_screen;
 
 var round_text, score_text, prompt_text, timewarp_text;
-var round, score, num_timewarp;
+var round, score, prompt_content, num_timewarp;
 
 var time_warp_button, restart_button;
+var cursors;
+
+var clickedx, clickedy;
 
 
-function createMarines() {
-    for(var y = 0; y < 4; y++) {
-        for(var x = 0; x < 4; x++) {
+
+
+function createMarines(row, col) {
+
+    for(var y = 0; y < col; y++) {
+        for(var x = 0; x < row; x++) {
             var marine = marines.create(x * 35, y * 35, 'marine');
             marine.alpha = 0.5
             marine.anchor.setTo(0.5, 0.5);
@@ -33,13 +35,18 @@ function createMarines() {
     marines.setAll('body.minBounceVelocity', 0);
 }
 
-function createBanelings() {
+function selected(sprite, pointer) {
+    sprite.alpha = 1;
+}
+
+function createBanelings(num_active) {
+
     banelings.createMultiple(100, 'baneling');  
     banelings.setAll('anchor.x', 0.5);
     banelings.setAll('anchor.y', 0.5);
     banelings.setAll('outOfBoundsKill', true);
 
-    for(var i = 0; i < 4; i ++) { 
+    for(var i = 0; i < num_active; i ++) { 
         var baneling = banelings.getFirstDead(); 
         baneling.reset(0, i * 30);
 
@@ -50,59 +57,50 @@ function createBanelings() {
     }
 }
 
-function checkNextRound() {
-    if (marines.countLiving() == 0) {
-        prompt_text.content = "No More Marines";
-        restart();
-    }
-    if( banelings.countLiving() == 0 && marines.countLiving() ) {
-        prompt_text.content = "Next Round...";
-        advanceRound();
-    }
-}
+// Num Explosions are equal to the number of active banelings on screen
+function createExplosions(num_explosions) {
 
-function advanceRound() {
-    console.log("advanceRound being called");
-    round += 1;
-    score += marines.countLiving() - 1;
-    console.log(round);
-    console.log(score);
-    score_text.content = "Score: " + score;
-    round_text.content = "Round: " + round;
-    restart();
-}
-
-function restart() {
-    console.log("Game should restart");
-
-    // marines.removeAll();
-    // banelings.removeAll();
-    // explosions.removeAll();
-
-    // createMarines();
-    // createBanelings();
-    // createExplosions();
-
-
-    // round_text.destroy();
-    // score_text.destroy();
-    // timewarp_text.destroy();
-    // prompt_text.destroy();
-
-    // createText();
-
-    // create();
-}
-
-
-
-function createExplosions() {
-    for (var i = 0; i < 4; i++) {
+    for (var i = 0; i < num_explosions; i++) {
         var explosionAnimation = explosions.create(0, 0, 'boom', [0], false);
         explosionAnimation.anchor.setTo(0.5, 0.5);
         explosionAnimation.animations.add('boom');
     }
+
 }
+
+function checkNextRound() {
+
+    if (marines.countLiving() == 0) {
+        prompt_content = "Restarting Round...";
+        restart();
+    }
+    else if( banelings.countLiving() == 0 && marines.countLiving() ) {
+        prompt_content = "Next Round...";
+        advanceRound();
+        restart();
+    }
+
+}
+
+function advanceRound() {
+    
+    console.log("advanceRound()");
+
+    round += 1;
+    score += marines.countLiving();
+}
+
+function restart() {
+    
+    console.log("restart()");
+
+    banelings.removeAll();
+    createBanelings(4);
+}
+
+
+
+
 
 function createText() {
     round = 1;
@@ -117,8 +115,17 @@ function createText() {
     timewarp_text = game.add.text(625, 550, "Time Warps:" + num_timewarp, { font: "24px Arial", fill: "#ff0044" });
     timewarp_text.anchor.setTo(0.5, 0.5);
 
-    prompt_text = game.add.text(600, 24, "PROMPT AREA", { font: "24px Arial", fill: "#ff0044" });
+    prompt_content = "PROMPT AREA";
+    prompt_text = game.add.text(600, 24, prompt_content, { font: "24px Arial", fill: "#ff0044" });
     prompt_text.anchor.setTo(0.5, 0.5);
+}
+
+function updateText() {
+    score_text.content = "Score: " + score;
+    round_text.content = "Round: " + round;
+    timewarp_text.content = "Time Warps: " + num_timewarp;
+    prompt_text.content = prompt_content;
+
 }
 
 function destoryAllText() {
@@ -128,9 +135,7 @@ function destoryAllText() {
     prompt_text.destroy();
 }
 
-function selected(sprite, pointer) {
-    sprite.alpha = 1;
-}
+
 
 function preload() {
      game.load.image('background','assets/green_cup.png');
@@ -144,18 +149,18 @@ function create() {
 	background = game.add.tileSprite(0, 0, 800, 600, 'background');
 
     marines = game.add.group();
-    createMarines();
+    createMarines(4, 4);
 
     banelings = game.add.group();
-    createBanelings();
+    createBanelings(4);
 
     explosions = game.add.group();
-    createExplosions();
+    createExplosions(4);
 
     createText();
 
-    // cursors = game.input.keyboard.createCursorKeys();
 
+    // cursors = game.input.keyboard.createCursorKeys();
     time_warp_button = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     time_warp_button.onDown.add(timeWarp, this);
 
@@ -168,10 +173,13 @@ function create() {
 function banelingHitMarine(baneling, marine) {
     baneling.kill();
     marine.kill();
+
     var explosionAnimation = explosions.getFirstDead();
     explosionAnimation.reset(marine.x, marine.y);
     explosionAnimation.play('boom', 30, false, true);
 }
+
+
 
 function timeWarp() {
     if(num_timewarp > 0) {
@@ -185,15 +193,22 @@ function timeWarp() {
 
 function update() {
     game.physics.collide(marines);
-    // game.physics.overlap(banelings);
     this.game.physics.overlap(marines, banelings, banelingHitMarine, null, this); 
     this.game.physics.overlap(marines, explosions, banelingHitMarine, null, this); 
 
+    updateText();
 }
 
 function render () {
 
 }
+
+
+
+
+
+
+
 
 
 
