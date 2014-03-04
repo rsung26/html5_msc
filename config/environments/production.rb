3d038bcebc3869e513c27bf0893e51c7a37bba70
rsung26 <rsung26@gmail.com>
2014-03-04 13:11:25 -0500

    added js files to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Html5Msc::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( main.js styles.css)
+  config.assets.precompile += %w( main.js drag_select.js attack_range.js styles.css)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
